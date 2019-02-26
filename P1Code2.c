6b4556ad014f2a3e4869748164ee09a8062bf8d6
rglapa <ruben.glapa@icloud.com>
2019-02-26 15:55:47 -0800

    Tue Feb 26 15:55:47 PST 2019

--- P1Code2.c
+++ P1Code2.c
@@ -2,8 +2,9 @@
 #include "unistd.h"
 #include "stdlib.h"
 
-int main() {
+int main(void) {
     printf("Line 1 ..\n");
+    fflush(stdout);
     write(1,"Line 2 ", 7);
     printf("\n");
 }
---
+++ P1Code5.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include "unistd.h"
+#include "stdlib.h"
+
+int main() {
+    int pid;
+    int i;
+    for(i=0;i<3;i++) {
+        if((pid=fork())<0) {
+            printf("Sorry...cannot fork\n");
+        } else if(pid == 0) {
+            printf("child %d\n", i);
+            fflush(stdout);
+        } else {
+            wait(0);
+            printf("parent %d\n", i);
+            fflush(stdout);
+        }
+    }
+    exit(0);
+}
